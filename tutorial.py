a7f37f479edfa8b8369716464bd9e8ed674715e5
Ignacio Archondo <archondo.ignacio@bcg.com>
2020-01-21 15:36:21 +0100

    Cleaned tutorial

--- tutorial.py
+++ tutorial.py
@@ -19,10 +19,3 @@ football.who_has_ball()["player"].calculate_passing_probabilities()
 # type(football.schedule.agents[football.id_dict["A_2"]])
 
 football.plot_grid()
-
-
-points = [(1, 0.9), (max_distance, 0.1)]
-x_coords, y_coords = zip(*points)
-A = vstack([x_coords, ones(len(x_coords))]).T
-m, c = lstsq(A, y_coords)[0]
-print("Line Solution is y = {m}x + {c}".format(m=m, c=c))
